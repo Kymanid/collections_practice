904c8bee54bf40af37b18579b8e64c47daaec0e7
Kymani Davis <Kymanidavis@gmail.com>
2020-11-22 23:19:02 -0500

    commit tree 1ed728e7f2888c80a79c8f5dce2eea3e470018e2

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(int)
+    int.sort do |a, b|
+        a <=> b
+    end
+end
+
+def sort_array_desc(int)
+    int.sort do |a, b|
+        b <=> a
+    end
+end
+
+def sort_array_char_count(int)
+    int.sort do |a, b|
+        a.length<=> b.length
+    end
+end
+
+def swap_elements(array, index1 = 1, index2 = 2)
+    element = array[index1]
+    array[index1] = array[index2]
+    array[index2] = element
+    array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(strings)
+    strings.each {|k| k[2] = "$"}
+    return strings
+end
+
+def find_a(array, start_char = 'a')
+    array.select { |string| string[0] == start_char }
+end
+
+def sum_array(array)
+    total = 0
+    array.each { |num| total += num }
+    total
+end
+
+def add_s (array)
+    array.collect.with_index do |word, index|
+        if index == 1
+            word
+        else
+            word + "s"
+        end
+    end
+end 
 
